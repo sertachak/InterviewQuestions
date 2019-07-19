e30d706c3f6936c20989fdc0a7a6aabd22c75913
sertachak <sertaconurhakbilen@gmail.com>
2019-07-19 23:39:31 +0300

    Jul 19 Day 3

--- creatingLinkedList.js
+++ creatingLinkedList.js
@@ -1,3 +1,11 @@
+class Node{
+  constructor( value ){
+    this.value = value;
+    this.next =  null;
+    this.prev = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -9,19 +17,33 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-      prev : this.tail
-    }
+    const newNode = new Node( value );
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
+  }
+
+    prepend(value) {
+    const newNode = new Node( value );
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  printLinkedList(){
+    const arr = [];
+    for( let curr = this.head; curr !== null; curr = curr.next )
+    {
+      arr.push(curr.value);
+    }
+    return arr;
   }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-
-console.log( myLinkedList);
+myLinkedList.prepend(1);
+//console.log( myLinkedList );
+myLinkedList.printLinkedList();
