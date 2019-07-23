3ab335d10b17eac9659f3e3a55aa23f4dfa9da49
sertachak <sertaconurhakbilen@gmail.com>
2019-07-23 22:32:33 +0300

    23 Jul Day 7

--- binarySearchTreeImp.js
+++ binarySearchTreeImp.js
@@ -1,8 +1,8 @@
 class Node {
   constructor(value){
+    this.value = value;
     this.left = null;
     this.right = null;
-    this.value = value;
   }
 }
 
@@ -11,6 +11,63 @@ class BinarySearchTree {
     this.root = null;
   }
   insert(value){
-
+    const newLeaf = new Node( value );
+    if(this.root === null ){
+      this.root = newLeaf;
+    }
+    else{
+      let currentNode = this.root;
+      while( true )
+      {
+        if( value < currentNode.value )
+        {
+          if( currentNode.left === null )
+          {
+            currentNode.left = newLeaf;
+            return this;
+          }
+          else{
+            currentNode = currentNode.left;
+          }
+        }
+        else{
+          if( currentNode.right === null )
+          {
+            currentNode.right = newLeaf;
+            return this;
+          }
+          else{
+            currentNode = currentNode.right;  
+          }
+        }
+      }
+      
+    }
   }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+JSON.stringify(traverse(tree.root))
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+
+
+
+
+
