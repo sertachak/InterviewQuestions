ce596b48f8eb437d269317d3344d475bcf6b6dbb
sertachak <sertaconurhakbilen@gmail.com>
2019-07-26 23:48:58 +0300

    Jul 26 Day 9

--- binarySearchTreeImp.js
+++ binarySearchTreeImp.js
@@ -44,6 +44,24 @@ class BinarySearchTree {
       
     }
   }
+
+    lookup(value){
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while(currentNode){
+      if(value < currentNode.value){
+        currentNode = currentNode.left;
+      } else if(value > currentNode.value){
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return null
+  }
+
 }
 
 const tree = new BinarySearchTree();
@@ -54,7 +72,10 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
-JSON.stringify(traverse(tree.root))
+//JSON.stringify(traverse(tree.root))
+tree.lookup(15);
+tree.lookup(7);
+
 
 //     9
 //  4     20
