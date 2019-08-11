c4bd3f13425c5cbd625f3a4c560cb81edae75ef9
sertachak <sertaconurhakbilen@gmail.com>
2019-08-11 11:56:41 +0300

    Aug 11 Day 23

---
+++ sortAlgorithmsForLanguages.js
@@ -0,0 +1,10 @@
+/* Sort algorithm for languages:
+Javascript => according to ES it can be differ by which program it is used. 
+E.g. Mozilla uses merge sort because of V8 engine. However Safari and Chrome has their own engine so they can be change it, 
+currently they are using quick sort for big data and insertion sort for small data.
+
+C++ -> C++ has sort() and qsort() algorithm. Qsort is a quicksort algorithm it takes comparator, 
+element array and pointer to that array. Sort algorihm is a hybrid algorithm which is introSort.
+ It is a hybrid of quicksort and heapsort which is followed by a insertion sort on the result.
+
+ */
