46f07dc22bd70c95317120e9202deda251f120b1
sertachak <sertaconurhakbilen@gmail.com>
2019-08-21 23:38:53 +0300

    Aug 21 Day 32

---
+++ depthFirstSearch.js
@@ -0,0 +1,10 @@
+//depthFirstSearch preparation
+
+
+//     9
+//  4     20
+//1  6  15  170
+
+//InOrder - [ 1, 4, 6, 9, 15, 20, 170 ]
+//PreOrder - [ 9, 4, 1, 6, 20, 15, 170 ] best usage for recreating tree
+//PostOrder - [ 1, 6, 4, 15, 170, 20 ,9 ]
