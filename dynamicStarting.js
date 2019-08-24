3fdc77b0fbee61fe310db6d194d313f12adbb712
sertachak <sertaconurhakbilen@gmail.com>
2019-08-24 22:18:42 +0300

    Aug 24 Day 34

---
+++ dynamicStarting.js
@@ -0,0 +1,32 @@
+function addTo80(n) {
+  return n + 80;
+}
+
+addTo80(5)
+
+let cache = {};
+function memoizeAddTo80(n) {
+  if (n in cache) {
+    return cache[n];
+  } else {
+    console.log('long time');
+    const answer = n + 80;
+    cache[n] = answer;
+    return answer;
+  }
+}
+
+//same function with closure
+function memoizeAddTo80(n) { 
+  let cache = {};
+  return function(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      console.log('long time');
+      const answer = n + 80;
+      cache[n] = answer;
+      return answer;
+    }
+  }
+}
