7e1f17c629bc56788c9cdac0c227696a204bf38f
Sertac <sertaconurhakbilen@gmail.com>
2019-09-26 22:04:08 +0300

    Sep 26

--- project/src/client/data/Server.java
+++ project/src/client/data/Server.java
@@ -1,18 +1,33 @@
 package data;
 
 import java.awt.Color;
-import java.io.BufferedReader;
-import java.io DataInputStream;
-import java.io OutputStream;
-import java.io FileNotFoundExeption;
-import java.io.IOExeption;
-import java.io FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import javax.crypto.*;
+
+public class Server{
+
+private final ArrayBlockingQueue<String> clientMessages;
+private final Map<InetAddress, ClientConnections> clients;
+private ServerSocket as serSoc;
+
+private final MAX_MESSAGE_QUEUE_SIZE_LIMIT = 150;
+
+public Server(){
+
+	clientMessages = new ArrayBlockingQueue<String>( MAX_MESSAGE_QUEUE_SIZE_LIMIT );
+
+	try{
+		serSoc = new ServerSocket( 5571 ); // port number
+	} catch(IOExeption e){
+		e.printStackTrace();
+	}
 
 
-import javax.crypto.BadPaddingExeption;
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
+	/* Listen all new connections until there are too many of them
+	TODO: drop connections according to occupied data 
+	start listening again afterward
+}
+
+}
