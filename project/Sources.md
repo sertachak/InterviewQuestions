220ec07995a18bc6b0a2d408406818d36c2e3d08
Sertac <sertaconurhakbilen@gmail.com>
2019-09-28 23:25:27 +0300

    Sep 28

--- project/Sources.md
+++ project/Sources.md
@@ -1,3 +1,5 @@
 #Sources
 
 https://searchsecurity.techtarget.com/definition/Advanced-Encryption-Standard
+
+https://docs.oracle.com/javase/tutorial/networking/sockets/clientServer.html
--- project/src/client/data/Server.java
+++ project/src/client/data/Server.java
@@ -9,25 +9,60 @@ import javax.crypto.*;
 public class Server{
 
 private final ArrayBlockingQueue<String> clientMessages;
-private final Map<InetAddress, ClientConnections> clients;
+private final Map<InetAddress, TargetConection> clients;
 private ServerSocket as serSoc;
 
-private final MAX_MESSAGE_QUEUE_SIZE_LIMIT = 150;
+private static final MAX_CONNECTION_SIZE = 10;
+private static final MAX_WAITING = 30;
+private static final MAX_MESSAGE_QUEUE_SIZE_LIMIT = 150;
 
 public Server(){
 
 	clientMessages = new ArrayBlockingQueue<String>( MAX_MESSAGE_QUEUE_SIZE_LIMIT );
 
 	try{
-		serSoc = new ServerSocket( 5571 ); // port number
+		serSoc = new ServerSocket( 5571 ); // port number when running a server choose a port that not already dedicated to some other service. 
+		//A new socket should be connected to serverSocket to listen server.
 	} catch(IOExeption e){
 		e.printStackTrace();
 	}
 
 
+	// Socket Represents the client side  serverSocket represents the server side
 	/* Listen all new connections until there are too many of them
 	TODO: drop connections according to occupied data 
-	start listening again afterward
+	start listening again afterward*/
+
+	Thread processMes = new Thread(){
+		@Override
+		public void run(){
+			while( true ){
+				try{
+					String message = clientMessages.take();
+					System.out.println( message );
+				}catch( InterruptedException e ){
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	Thread connectionWithProtection = new Thread(){
+		@Override
+		public void run(){
+			while ( activeConnections < MAX_CONNECTION_SIZE ){
+				try{
+					Socket soc = serSoc.accept();
+					TargetConection target = new TargetConection(GREEN, Server.this, soc );
+				}catch(IOExeption e){
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
+
+
 
 }
