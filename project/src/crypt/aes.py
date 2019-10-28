7d5fbeb8aa4558b435356580ed2827d686c5b6c2
Sertac <sertaconurhakbilen@gmail.com>
2019-10-28 22:38:11 +0300

    Oct 28-2

--- project/src/crypt/aes.py
+++ project/src/crypt/aes.py
@@ -4,15 +4,14 @@ import os, random, string, struct
 
 
 def randomword(length):
-    #chars = string.lowercase()+string.digits+string.uppercase()
-    #return ''.join(random.choice(chars) for i in range(length))
-    return '' #Todo problem with string
+    chars = string.ascii_lowercase+string.digits+string.ascii_uppercase
+    return ''.join(random.choice(chars) for i in range(length))
 
 
 password = randomword( 64 )
 salt = os.urandom(8)
 
-key = PBKDF2( password, salt) # AES key must be either 16, 24, or 32 bytes long
+key = PBKDF2( password, salt).read(32) # AES key must be either 16, 24, or 32 bytes long
 IV = 16 * '\x01'           # TODO IV must be 16 bytes long initialization vector for maximize security IV should be randomly generated for every encryption in can be stored with ciphered text.
 mode = AES.MODE_CBC
 encryptor = AES.new(key, mode, IV=IV)
