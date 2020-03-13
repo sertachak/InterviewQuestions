1a61ca45493959cd34f9a2afe0344e4ef0f0f4a0
Sertac <sertaconurhakbilen@gmail.com>
2020-03-14 01:21:51 +0300

    Chat with basics

---
+++ chat/client/src/components/Chat/Chat.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
+
+const Chat = ({ location }) => {
+
+    const[name, setName] = useState('');
+    const[room, setRoom] = useState('');
+    const CONNECTION = 'localhost:5000';
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+        socket = io(CONNECTION);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', {name, room}, ({error}) => {
+            console.log(error);
+        });
+
+    }, [location.search, CONNECTION])
+
+    return( 
+        <h1>
+            Chat
+        </h1>
+   )
+}
+
+export default Chat;
---
+++ chat/server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection !!!!!!!!!!!!!!!!!!!');
+
+    socket.on('join', ({name, room}, turnback) => {
+        console.log(name,room);
+
+        const error = true;
+
+        if(error){
+
+            turnback({error:'error'});
+
+        }
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User left');
+    })
+})
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started port : ${PORT}`) );
