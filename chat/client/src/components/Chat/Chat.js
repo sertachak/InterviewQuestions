cc844d5fa8f8cb3663723e348e7e364d341c7af0
Sertaç Onur Hakbilen <tr1h5045@icterra.net>
2020-03-20 22:30:35 +0300

    Adding components to chat page material-ui

--- chat/client/src/components/Chat/Chat.js
+++ chat/client/src/components/Chat/Chat.js
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import './Chat';
+
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles();
+
 let socket;
 
 const Chat = ({ location }) => {
-
+    const classes = makeStyles();
     const[name, setName] = useState('');
     const[room, setRoom] = useState('');
     const[message, setMessage] = useState('');
@@ -45,7 +56,18 @@ const Chat = ({ location }) => {
     console.log(message, messages);
 
     return( 
-        <div>
+        <div className="headerBar">
+            <AppBar>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
             <div>
                 <input 
                 value={message} 
